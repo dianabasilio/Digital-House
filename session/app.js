84fde82b288cb7c06ec669feb8b222c50f432e07
dianabasilio <dianabasilio99@gmail.com>
2022-01-31 22:34:02 -0600

    Ejemplo pequeño de como usar session y cookie

---
+++ session/app.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const app = express();
+
+const session = require('express-session');
+app.use(session({secret: "frase secreta"}));
+
+const cambiarIdioma = (req, res) => {
+	// Escribir tu código aquí
+	req.session.idioma= (req.query.idioma);
+
+	res.redirect('/');
+}
+
+const express = require('express');
+const app = express();
+
+const session = require('express-session');
+app.use(session({secret: "frase secreta"}));
+
+const admin = (req, res) => {
+    if(!req.session.admin){
+        res.redirect('/login')
+    }
+}
+
+//Para eso, dentro del controlador, vamos a usar el método cookie del objeto response. Este método espera, 
+//como primer parámetro, el nombre de la cookie,
+// en este caso "ultimoAcceso". El segundo parámetro va a ser la fecha de acceso, esto lo podemos obtener haciendo new Date().
+
+var express = require('express');
+var cookieParser = require('cookie-parser');
+
+var app = express();
+app.use(cookieParser());
+
+const index= (req, res) => {
+	//Escribí tu código acá
+	res.cookie('ultimoAcceso', new Date());
+
+	//Código del controlador
+	const cambiarIdioma = (req, res) => {
+	// Escribir tu código aquí
+	req.session.idioma= (req.query.idioma);
+
+	res.redirect('/');
+}
+}
+
+//COOKIES
+var express = require('express');
+var cookieParser = require('cookie-parser');
+
+var app = express();
+app.use(cookieParser());
+
+const index = (req, res) => {
+	let estilo;
+
+	//Agregá el if elses acá
+	 if (req.cookies.estilo != undefined){
+estilo=req.cookies.estilo;
+	 }else{
+	estilo="default";
+
+	 }
+
+	res.render('/', {estilo: estilo});
+
+	//Código del controlador
+}
